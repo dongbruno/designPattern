8e5b1c4f8e46cafb0dbccee9a940a91d5fc0af80
Bruno Dong <1162961485@qq.com>
2017-11-07 22:50:47 +0800

    proxy pattern

---
+++ TransferObjectPattern/src/TransferObjectPatternDemo.java
@@ -0,0 +1,27 @@
+public class TransferObjectPatternDemo {
+
+	public static void main(String[] args) {
+		StudentBO studentBusinessObject = new StudentBO();
+		
+		// print all students
+		for(StudentVO student: studentBusinessObject.getAllStudents()) {
+			System.out.println("Student: [RollNo: "+student.getRollNo()+
+					", Name: "+ student.getName()+"]");
+		}
+		// update student
+		StudentVO student = studentBusinessObject.getAllStudents().get(0);
+		student.setName("Michael");
+		// get the student
+		student = studentBusinessObject.getStudent(0);
+		System.out.println("before updated: Student: [RollNo:" + student.getRollNo() + ", Name:" + student.getName() + "]");
+		
+		studentBusinessObject.updateStudents(student);
+
+		//get the student
+		student = studentBusinessObject.getStudent(0);
+		System.out.println("after updated: Student: [RollNo:"+ student.getRollNo()+
+				", Name:"+student.getName()+"]");
+		
+		
+	}
+}
